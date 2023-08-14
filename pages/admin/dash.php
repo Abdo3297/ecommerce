bb7556839f00842338e418d264efc4e288d7dd31
abdelrhmanhassanmohamed <abdelrhmanhassanmohamed@gmail.com>
2023-08-15 01:59:20 +0300

    update

--- pages/admin/dash.php
+++ pages/admin/dash.php
@@ -13,25 +13,47 @@ if (!(isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin')) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="h-full">
     <div class="min-h-full">
+        <!-- navbar -->
         <?php include('../../components/nav.php'); ?>
-
+        <!-- header -->
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <h1 class="text-center text-3xl font-bold tracking-tight text-gray-900">Dashboard , Hello <span class="text-red-600"><?= $_SESSION['user']['username'] ?></span></h1>
             </div>
         </header>
+        <!-- modal to add product -->
 
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
             </div>
-        </main>
+        </div>
+
     </div>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
     <script src="./assets/js/script.js"></script>
 </body>
 
