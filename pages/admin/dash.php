5cf10fe8fc530b905b2bf2ddd0474a3c0936ae94
abdelrhmanhassanmohamed <abdelrhmanhassanmohamed@gmail.com>
2023-08-15 01:25:42 +0300

    update

--- pages/admin/dash.php
+++ pages/admin/dash.php
@@ -22,9 +22,10 @@ if (!(isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin')) {
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard , Hello <?= $_SESSION['user']['username'] ?></h1>
+                <h1 class="text-center text-3xl font-bold tracking-tight text-gray-900">Dashboard , Hello <span class="text-red-600"><?= $_SESSION['user']['username'] ?></span></h1>
             </div>
         </header>
+
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
--- pages/admin/profile.php
+++ pages/admin/profile.php
@@ -34,13 +34,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>profile</title>
+    <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        form#profile {
+            max-width: 480px;
+            width: 100%;
+            padding: 1rem;
+            border: 1px solid #ccc;
+            border-radius: 0.5rem;
+        }
+    </style>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-        <?php include('../../components/nav.php'); ?>
+    <?php include('../../components/nav.php'); ?>
 
-
+    <div class="container">
         <form id="profile" method="post">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
@@ -51,19 +67,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <div class="sm:col-span-4">
                             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
                             <div class="mt-2">
-                                <input value="<?= $_SESSION['user']['username']?>" id="username" name="username" type="text" autocomplete="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input value="<?= $_SESSION['user']['username'] ?>" id="username" name="username" type="text" autocomplete="text" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div class="sm:col-span-4">
                             <label for="Email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
                             <div class="mt-2">
-                                <input value="<?= $_SESSION['user']['email']?>" id="Email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input value="<?= $_SESSION['user']['email'] ?>" id="Email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div class="sm:col-span-4">
                             <label for="Password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                             <div class="mt-2">
-                                <input value="<?= $_SESSION['user']['password']?>" id="Password" name="password" type="password" autocomplete="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input value="<?= $_SESSION['user']['password'] ?>" id="Password" name="password" type="password" autocomplete="password" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                     </div>
@@ -72,11 +88,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <a href="http://localhost/ecommerce/pages/admin/dash.php" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-                <button value="<?=$_SESSION['user']['id'];?>" type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" name="update">Save</button>
+                <button value="<?= $_SESSION['user']['id']; ?>" type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" name="update">Save</button>
             </div>
         </form>
+    </div>
 
-        <script src="./assets/js/script.js"></script>
+
+    <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
